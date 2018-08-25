184b8565928e1d84923d8917ed7b427f98ff16e8
Leyton Blackler <leytonblackler@gmail.com>
2018-08-25 17:21:35 +1200

    Parks now display on the map as square polygons.

--- client/src/MapComponent.js
+++ client/src/MapComponent.js
@@ -1,32 +1,42 @@
-import {withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
-import React from "react"
-import {Polygon} from "react-google-maps"
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from 'react-google-maps'
+import React from 'react'
+import { Polygon } from 'react-google-maps'
+import parking_data from './data/parking_data.json';
 
+export const MapComponent = withScriptjs(
+  withGoogleMap(props => (
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -41.2865, lng: 174.7762 }}>
+      {props.isMarkerShown &&
+        <Marker position={{ lat: -41.2865, lng: 174.7762 }} />}
 
-const coords = [
-  {lat: -41.251470, lng: 174.704399},
-  {lat: -41.285118, lng: 174.743470},
-  {lat: -41.282696, lng: 174.682161}
-]
+      {parking_data.map(function (park) {
+        const size = 0.00001;
+        console.log(park.geometry);
+        const coords = [
+          { lat: park.geometry.y + size, lng: park.geometry.x + size },
+          { lat: park.geometry.y + size, lng: park.geometry.x - size },
+          { lat: park.geometry.y - size, lng: park.geometry.x - size },
+          { lat: park.geometry.y - size, lng: park.geometry.x + size }
+        ]
+        return (
+          <Polygon
+            path={coords}
+            key={1}
+            options={{
+              fillColor: '#FF5722',
+              fillOpacity: 0.4,
+              strokeWeight: 0
+            }}
+            onClick={() => console.log('yes')}
+          ></Polygon>
+        )
+      })}
 
-export const MapComponent = withScriptjs(withGoogleMap((props) =>
-
-  <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: -41.2865, lng: 174.7762}}
-  >
-    {props.isMarkerShown && <Marker position={{ lat: -41.2865, lng: 174.7762 }} />}
-    <Polygon path = {coords}
-      key = {1}
-    options = {{
-      fillCollor: "#000",
-      fillOpacity: 0.4
-    }}
-    onClick = {() =>
-      console.log("yes")
-    }
-    />
-
-
-  </GoogleMap>
-))
+    </GoogleMap>
+  ))
+)
