25e52f045f49f441d24ac497457ecdcd574ffa6e
Sam <samong0105@gmail.com>
2018-08-26 10:10:04 +1200

    Got the heat map data working

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 import './App.css';
 import {MapComponent} from './MapComponent';
@@ -30,7 +31,7 @@ class App extends Component {
       <div className="App">
       <MapComponent
       isMarkerShown
-      googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCm_yPi4u2iAfSTSR-lAsrdWZHN-NbuIMI"
+      googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places,visualization&key=AIzaSyCm_yPi4u2iAfSTSR-lAsrdWZHN-NbuIMI"
       loadingElement={<div style={{ height: `100%` }} />}
       containerElement={<div style={{ height: `400px` }} />}
       mapElement={<div style={{ height: `100%` }} />}
--- client/src/MapComponent.js
+++ client/src/MapComponent.js
@@ -1,15 +1,19 @@
+/*global google*/
 import {
   withScriptjs,
   withGoogleMap,
   GoogleMap,
-  Marker
+  Marker,
+
 } from 'react-google-maps'
 import React from 'react'
-import { Polygon } from 'react-google-maps'
+import {Polygon} from 'react-google-maps'
+import HeatmapLayer from 'react-google-maps/lib/components/visualization/HeatmapLayer'
 import parking_data from './data/parking_data.json';
 
-export const MapComponent = withScriptjs(
-  withGoogleMap(props => (
+<script src= "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places,visualization&key=AIzaSyCm_yPi4u2iAfSTSR-lAsrdWZHN-NbuIMI"></script>
+
+export const MapComponent = withScriptjs( withGoogleMap(props => (
     <GoogleMap defaultZoom={8} defaultCenter={{ lat: -41.2865, lng: 174.7762 }}>
       {props.isMarkerShown &&
         <Marker position={{ lat: -41.2865, lng: 174.7762 }} />}
@@ -37,6 +41,18 @@ export const MapComponent = withScriptjs(
         )
       })}
 
+      <HeatmapLayer
+      data= {getHeatMapData()}
+      />
+
     </GoogleMap>
   ))
 )
+
+function getHeatMapData(){
+  let data = []
+  parking_data.map(function (park){
+    data.push(new google.maps.LatLng(park.geometry.y, park.geometry.x))
+  })
+  return data
+}
