dd292e9babc23173b378d35cb929779b1609285c
Leyton Blackler <leytonblackler@gmail.com>
2018-08-26 11:11:10 +1200

    Added a search box to the map.

--- client/src/MapComponent.js
+++ client/src/MapComponent.js
@@ -1,26 +1,78 @@
-/*global google*/
+/* global google */
 import {
   withScriptjs,
   withGoogleMap,
   GoogleMap,
-  Marker,
-
+  Marker
 } from 'react-google-maps'
 import React from 'react'
-import {Polygon} from 'react-google-maps'
-import HeatmapLayer from 'react-google-maps/lib/components/visualization/HeatmapLayer'
-import parking_data from './data/parking_data.json';
+import { Polygon } from 'react-google-maps'
+import HeatmapLayer
+  from 'react-google-maps/lib/components/visualization/HeatmapLayer'
+import parking_data from './data/parking_data.json'
+import { StandaloneSearchBox } from 'react-google-maps/lib/components/places/StandaloneSearchBox'
+;<script src='https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places,visualization&key=AIzaSyCm_yPi4u2iAfSTSR-lAsrdWZHN-NbuIMI' />
 
-<script src= "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places,visualization&key=AIzaSyCm_yPi4u2iAfSTSR-lAsrdWZHN-NbuIMI"></script>
+export const MapComponent = withScriptjs(
+  withGoogleMap(props => (
 
-export const MapComponent = withScriptjs( withGoogleMap(props => (
-    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -41.2865, lng: 174.7762 }}>
+    
+
+    <GoogleMap
+      options={{
+        disableDefaultUI: true,
+        zoomControl: true,
+        zoomControlOptions: {
+          style: google.maps.ZoomControlStyle.LARGE
+        }
+      }}
+      defaultZoom={11}
+      defaultCenter={{ lat: -41.2865, lng: 174.7762 }}
+    >
       {props.isMarkerShown &&
         <Marker position={{ lat: -41.2865, lng: 174.7762 }} />}
 
+      <div
+        style={{
+          display: 'fixed',
+          top: '0',
+          width: '100vw',
+          height: '10vh',
+          backgroundColor: '#FF0000'
+        }}
+      >
+      <StandaloneSearchBox
+          ref={props.onSearchBoxMounted}
+          bounds={props.bounds}
+          controlPosition={google.maps.ControlPosition.TOP_LEFT}
+          onPlacesChanged={props.onPlacesChanged}
+        >
+          <input
+            type='text'
+            placeholder=''
+            style={{
+              boxSizing: `border-box`,
+              border: `1px solid transparent`,
+              width: `240px`,
+              height: `32px`,
+              marginTop: `27px`,
+              marginLeft: '27px',
+              padding: `0 12px`,
+              borderRadius: `3px`,
+              boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+              fontSize: `14px`,
+              outline: `none`,
+              textOverflow: `ellipses`,
+              backgroundColor: 'rgba(255,139,40,1)',
+              color: 'rgba(255,255,255,1)'
+            }}
+          />
+        </StandaloneSearchBox>
+      </div>
+
       {parking_data.map(function (park) {
-        const size = 0.00001;
-        console.log(park.geometry);
+        const size = 0.00001
+        console.log(park.geometry)
         const coords = [
           { lat: park.geometry.y + size, lng: park.geometry.x + size },
           { lat: park.geometry.y + size, lng: park.geometry.x - size },
@@ -37,21 +89,19 @@ export const MapComponent = withScriptjs( withGoogleMap(props => (
               strokeWeight: 0
             }}
             onClick={() => console.log('yes')}
-          ></Polygon>
+          />
         )
       })}
 
-      <HeatmapLayer
-      data= {getHeatMapData()}
-      />
+      <HeatmapLayer data={getHeatMapData()} />
 
     </GoogleMap>
   ))
 )
 
-function getHeatMapData(){
+function getHeatMapData () {
   let data = []
-  parking_data.map(function (park){
+  parking_data.map(function (park) {
     data.push(new google.maps.LatLng(park.geometry.y, park.geometry.x))
   })
   return data
