112ba5459053d26040ae97a10fe4c9cad3e9f1bf
Leyton Blackler <leytonblackler@gmail.com>
2018-08-26 16:31:35 +1200

    Made the website "responsive".

--- client/src/MapComponent.js
+++ client/src/MapComponent.js
@@ -8,133 +8,155 @@ import {
 import React from 'react'
 import { Polygon } from 'react-google-maps'
 import HeatmapLayer
-from 'react-google-maps/lib/components/visualization/HeatmapLayer'
+  from 'react-google-maps/lib/components/visualization/HeatmapLayer'
 import parking_data from './data/parking_data.json'
 import {
   StandaloneSearchBox
-} from 'react-google-maps/lib/components/places/StandaloneSearchBox';
+} from 'react-google-maps/lib/components/places/StandaloneSearchBox'
 
-import logo from './images/logo.png';
-
-<script src='https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places,visualization&key=AIzaSyCm_yPi4u2iAfSTSR-lAsrdWZHN-NbuIMI' />
+import logo from './images/logo.png'
+import arrow from './images/arrow-icon.png'
+;<script src='https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places,visualization&key=AIzaSyCm_yPi4u2iAfSTSR-lAsrdWZHN-NbuIMI' />
 
 export const MapComponent = withScriptjs(
   withGoogleMap(props => (
-    <GoogleMap
-    options={{
-      disableDefaultUI: true,
-      zoomControl: true,
-      zoomControlOptions: {
-        style: google.maps.ZoomControlStyle.LARGE
-      }
-    }}
-    defaultZoom={11}
-    defaultCenter={{ lat: -41.2865, lng: 174.7762 }}
-    ref = {(mapInstance) => {this.mapInstance = mapInstance}}
-    >
-
-    {props.isMarkerShown &&
-      <Marker position={{ lat: -41.2865, lng: 174.7762 }} />}
-
-      <div
-      style={{
-        position: 'fixed',
-        top: '0',
-        width: '100vw',
-        height: '10vh',
-        backgroundColor: 'rgba(255,139,40,1)',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-evenly',
-        alignItems: 'center',
-        boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-      }}
-      >
-
-      <img id="logo" src={logo} />
-      <input type="date" className= 'input' value= '2018-08-26'></input>
-      <input type="time" className= 'input' value= '19:15:00'></input>
-      <StandaloneSearchBox
-      ref = {(searchBox) => {this.searchBox = searchBox}}
-      bounds={props.bounds}
-      controlPosition={google.maps.ControlPosition.TOP_LEFT}
-      onPlacesChanged={() =>{
-        const bounds = new google.maps.LatLngBounds();
-        const places = this.searchBox.getPlaces();
-
-        places.forEach(place => {
-
-          if (place.geometry.viewport) {
-            bounds.union(place.geometry.viewport)
-          } else {
-            bounds.extend(place.geometry.location)
+    <div className='main-container'>
+      <GoogleMap
+        options={{
+          disableDefaultUI: true,
+          zoomControl: true,
+          zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.LARGE
           }
-        });
-        this.mapInstance.fitBounds(bounds)
-      }}
+        }}
+        defaultZoom={11}
+        defaultCenter={{ lat: -41.2865, lng: 174.7762 }}
+        ref={mapInstance => {
+          this.mapInstance = mapInstance
+        }}
       >
-      <input
-      className='input'
-      id = 'location-search'
-      type='text'
-      placeholder='Search for your destination'
 
-      />
-      </StandaloneSearchBox>
-      </div>
+        {props.isMarkerShown &&
+          <Marker position={{ lat: -41.2865, lng: 174.7762 }} />}
 
-      {parking_data.map(function (park) {
-        const size = 0.00001
-        console.log(park.geometry)
-        const coords = [
-          { lat: park.geometry.y + size, lng: park.geometry.x + size },
-          { lat: park.geometry.y + size, lng: park.geometry.x - size },
-          { lat: park.geometry.y - size, lng: park.geometry.x - size },
-          { lat: park.geometry.y - size, lng: park.geometry.x + size }
-        ]
-        return (
-          <Polygon
-          path={coords}
-          key={1}
-          options={{
-            fillColor: '#FF5722',
-            fillOpacity: 0.4,
-            strokeWeight: 0
+        <div className='sliding-menu-container'>
+          <StandaloneSearchBox
+            ref={searchBox => {
+              this.searchBox = searchBox
+            }}
+            bounds={props.bounds}
+            controlPosition={google.maps.ControlPosition.TOP_LEFT}
+            onPlacesChanged={() => {
+              const bounds = new google.maps.LatLngBounds()
+              const places = this.searchBox.getPlaces()
+
+              places.forEach(place => {
+                if (place.geometry.viewport) {
+                  bounds.union(place.geometry.viewport)
+                } else {
+                  bounds.extend(place.geometry.location)
+                }
+              })
+              this.mapInstance.fitBounds(bounds)
+            }}
+          >
+            <input
+              className='input'
+              id='location-search'
+              type='text'
+              placeholder='Search for your destination'
+            />
+          </StandaloneSearchBox>
+          <div className='date-time-container'>
+            <input type='date' className='input' value='2018-08-26' />
+            <input type='time' className='input' value='19:15:00' />
+          </div>
+          <div id='drop-button-container'>
+          <img id='arrow' src={arrow} onClick={function () {
+                var slider = document.getElementsByClassName(
+                  'sliding-menu-container'
+                )[0]
+                console.log(slider)
+                if (slider.classList.contains('active')) {
+                  slider.classList.remove('active')
+                } else {
+                  slider.classList.add('active')
+                }
+              }}/>
+          </div>
+        </div>
+
+        <div
+          style={{
+            position: 'fixed',
+            top: '0',
+            width: '100vw',
+            height: '60px',
+            backgroundColor: 'rgba(255,139,40,1)',
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+            alignItems: 'center',
+            boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`
           }}
-          onClick={() => console.log('yes')}
-          />
-        )
-      })}
+        >
 
-      <HeatmapLayer
-      data={getHeatMapData()}
-      options={{
-        radius: 20,
-        gradient: [
-          'rgba(255, 220, 13, 0)',
-          'rgba(255, 196, 16, 1)',
+          <img id='logo' src={logo} />
 
-          'rgba(255, 171, 19, 1)',
-          'rgba(255, 147, 22, 1)',
-          'rgba(255, 122, 25, 1)',
-          'rgba(255, 98, 28, 1)'
+        </div>
 
-        ]
-      }}
-      />
+        {parking_data.map(function (park) {
+          const size = 0.00001
+          console.log(park.geometry)
+          const coords = [
+            { lat: park.geometry.y + size, lng: park.geometry.x + size },
+            { lat: park.geometry.y + size, lng: park.geometry.x - size },
+            { lat: park.geometry.y - size, lng: park.geometry.x - size },
+            { lat: park.geometry.y - size, lng: park.geometry.x + size }
+          ]
+          return (
+            <Polygon
+              path={coords}
+              key={1}
+              options={{
+                fillColor: '#FF5722',
+                fillOpacity: 0.4,
+                strokeWeight: 0
+              }}
+              onClick={() => console.log('yes')}
+            />
+          )
+        })}
+
+        <HeatmapLayer
+          data={getHeatMapData()}
+          options={{
+            radius: 20,
+            gradient: [
+              'rgba(255, 220, 13, 0)',
+              'rgba(255, 196, 16, 1)',
+
+              'rgba(255, 171, 19, 1)',
+              'rgba(255, 147, 22, 1)',
+              'rgba(255, 122, 25, 1)',
+              'rgba(255, 98, 28, 1)'
+            ]
+          }}
+        />
 
       </GoogleMap>
-    ))
-  )
+    </div>
+  ))
+)
 
-  function onPlacesChanged(data){
-    console.log("places changed " + data)
-  }
+function onPlacesChanged (data) {
+  console.log('places changed ' + data)
+}
 
-  function getHeatMapData () {
-    let data = []
-    parking_data.map(function (park) {
-      data.push(new google.maps.LatLng(park.geometry.y, park.geometry.x))
-    })
-    return data
-  }
+function getHeatMapData () {
+  let data = []
+  parking_data.map(function (park) {
+    data.push(new google.maps.LatLng(park.geometry.y, park.geometry.x))
+  })
+  return data
+}
